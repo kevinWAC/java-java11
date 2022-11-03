fe763ca774c74286581691202ca8bdc025eb84c7
kevin <kevin.ulreich@epitech.eu>
2022-11-03 12:03:58 +0100

    stream

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -24,7 +24,9 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Trouver la liste des clients associés aux commandes
-		List<Customer> result = null;
+		List<Customer> result = orders.stream()
+				.map(o -> o.getCustomer())
+				.toList();
 
 		assertThat(result, hasSize(8));
 	}
@@ -35,7 +37,9 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre de clients associés aux commandes
-		long result = 0;
+		long result = orders.stream()
+				.map(o-> o.getCustomer())
+				.count();
 
 		assertThat(result, is(8L));
 	}
@@ -46,7 +50,10 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Trouver la liste des différents clients associés aux commandes (sans doublons)
-		List<Customer> result = null;
+		List<Customer> result = orders.stream()
+				.map(o -> o.getCustomer())
+				.distinct()
+				.toList();
 
 		assertThat(result, hasSize(2));
 	}
@@ -57,8 +64,11 @@ public class Stream_02_Test {
 		List<Order> orders = new Data().getOrders();
 
 		// TODO Compter le nombre des différents clients associés aux commandes
-		long result = 0L;
-
+		long result = orders.stream()
+				.map(o-> o.getCustomer())
+				.distinct()
+				.count();
+		
 		assertThat(result, is(2L));
 	}
 
@@ -71,7 +81,9 @@ public class Stream_02_Test {
 		 * TODO Calculer le chiffre d'affaires total de la pizzeria (somme des prix des
 		 * commandes)
 		 */
-		double result = 0.0;
+		double result = orders.stream()
+				.mapToDouble(o -> o.getPrice())
+				.sum();
 
 		assertThat(result, is(10900.0));
 	}
@@ -84,7 +96,9 @@ public class Stream_02_Test {
 		/*
 		 * TODO Calculer le prix moyen d'une commande
 		 */
-		OptionalDouble result = null;
+		OptionalDouble result = orders.stream()
+				.mapToDouble(o -> o.getPrice())
+				.average();
 
 		assertThat(result.isPresent(), is(true));
 		assertThat(result.getAsDouble(), is(1362.5));
